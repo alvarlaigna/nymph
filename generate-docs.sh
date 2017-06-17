8baf90363b1c80bdf03a4496d2d01a9aa84e0bdd
Hunter Perrin <hperrin@gmail.com>
2017-06-16 19:48:22 -0700

    Update API docs.

--- generate-docs.sh
+++ generate-docs.sh
@@ -2,4 +2,4 @@
 
 cd $(dirname $0)
 
-apigen generate -s ./server/ -d ./api/server --exclude vendor/ --exclude bower_components/ --title "Nymph Server API" --tree --main Nymph
+./vendor/apigen/apigen/bin/apigen generate -s ./server/ -d ./api/server --exclude vendor/ --exclude testing/ --exclude bower_components/ --exclude node_modules/ --title "Nymph Server API" --tree --main Nymph
